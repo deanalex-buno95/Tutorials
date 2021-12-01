0400693539e6dcce3562090694ae645223c01ff7
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-01 09:27:04 +0800

    Completed P4Q3.

--- Practical 04/subject_reader.py
+++ Practical 04/subject_reader.py
@@ -17,19 +17,26 @@ FUNCTIONS
 def main():
     """ The main function to output data from 'subject_data.txt' """
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
     """ Read data from 'subject_data.txt' file that is formatted as: subject,lecturer,number of students """
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)
-        print(repr(line))
-        line = line.strip()
-        parts = line.split(",")
-        print(parts)
-        print("-+-")
+        line = line.strip()  # strip '\n'
+        subject, lecturer, number_of_students = line.split(",")  # split line into multiple parts via ','
+        data_row = [subject, lecturer, int(number_of_students)]  # each data row in a list
+        data.append(data_row)  # append data row list into main data list
+    input_file.close()
+    return data
+
+
+def display_data(data):
+    """ Display data in terms of sentences """
+    for subject, lecturer, number_of_students in data:
+        print(f"{subject} is taught by {lecturer} and has {number_of_students} students.")
 
 
 """
