c6ceffffdfcfbf739ae1da56efb560f3c581d30c
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-06 21:34:05 +0800

    Added new Python file to Practical 05 directory, 'word_occurrences.py'.

---
+++ Practical 05/word_occurrences.py
@@ -0,0 +1,27 @@
+"""
+Practical 5: Question 3
+word_occurrences.py
+"""
+
+# CONSTANTS
+BORDER = "—" * 150
+
+
+"""
+MAIN FUNCTION
+"""
+
+
+def main():
+    """ Main function to count the word occurrences from a string """
+    print(BORDER)
+    print("Practical 5: Question 3\nword_occurrences.py")
+    print(BORDER)
+
+
+"""
+RUN MAIN FUNCTION
+"""
+
+if __name__ == '__main__':
+    main()
