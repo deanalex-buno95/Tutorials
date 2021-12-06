73c2e2e0a3e35c31e8552481b1188be390038dbe
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-06 21:19:09 +0800

    Added 10 colors to dictionary 'NAME_TO_CODE'.

--- Practical 05/hex_colours.py
+++ Practical 05/hex_colours.py
@@ -5,7 +5,7 @@ hex_colours.py
 
 # CONSTANTS
 BORDER = "—" * 150
-CODE_TO_NAME = {"Red": "FF0000", "Orange": "FFA500", "Yellow": "FFFF00", "Lime": "00FF00", "Green": "008000",
+NAME_TO_CODE = {"Red": "FF0000", "Orange": "FFA500", "Yellow": "FFFF00", "Lime": "00FF00", "Green": "008000",
                 "Teal": "008080", "Blue": "0000FF", "Purple": "800080", "Magenta": "FF00FF", "Pink": "FFC0CB"}
 
 
@@ -15,7 +15,7 @@ MAIN FUNCTION
 
 
 def main():
-    """ Main function to get colour name from colour code input from dictionary 'CODE_TO_NAME' """
+    """ Main function to get colour name from colour code input from dictionary 'NAME_TO_CODE' """
     print(BORDER)
     print("Practical 5: Question 2\nhex_colours.py")
     print(BORDER)
