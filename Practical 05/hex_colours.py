e7e9917447d62f072f987b319c55a40ad873fd9c
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-06 20:56:11 +0800

    Added new Python file in Practical 05, 'hex_colours.py'

---
+++ Practical 05/hex_colours.py
@@ -0,0 +1,28 @@
+"""
+Practical 5: Question 2
+hex_colours.py
+"""
+
+# CONSTANTS
+BORDER = "—" * 150
+CODE_TO_NAME = {}
+
+
+"""
+MAIN FUNCTION
+"""
+
+
+def main():
+    """ Main function to get colour name from colour code input from dictionary 'CODE_TO_NAME' """
+    print(BORDER)
+    print("Practical 5: Question 2\nhex_colours.py")
+    print(BORDER)
+
+
+"""
+RUN MAIN FUNCTION
+"""
+
+if __name__ == '__main__':
+    main()
