4ee4b4ed522456120173f11cdbb7518c5b222765
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-06 21:16:02 +0800

    Added 10 color codes to dictionary 'CODE_TO_NAME'.

--- Practical 05/hex_colours.py
+++ Practical 05/hex_colours.py
@@ -5,7 +5,8 @@ hex_colours.py
 
 # CONSTANTS
 BORDER = "—" * 150
-CODE_TO_NAME = {}
+CODE_TO_NAME = {"Red": "FF0000", "Orange": "FFA500", "Yellow": "FFFF00", "Lime": "00FF00", "Green": "008000",
+                "Teal": "008080", "Blue": "0000FF", "Purple": "800080", "Magenta": "FF00FF", "Pink": "FFC0CB"}
 
 
 """
