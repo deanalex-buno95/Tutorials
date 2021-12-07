d25a7e4d3c3a0dec22fa04e6c477333891b932a9
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-07 18:33:10 +0800

    Added new Python file to directory Practical 05, 'emails.py'

---
+++ Practical 05/emails.py
@@ -0,0 +1,27 @@
+"""
+Practical 5: Question 4
+emails.py
+"""
+
+# CONSTANTS
+BORDER = "—" * 150
+
+
+"""
+MAIN FUNCTION
+"""
+
+
+def main():
+    """ Main function to get emails and respective names into a dictionary """
+    print(BORDER)
+    print("Practical 5: Question 4\nemails.py")
+    print(BORDER)
+
+
+"""
+RUN MAIN FUNCTION
+"""
+
+if __name__ == '__main__':
+    main()
