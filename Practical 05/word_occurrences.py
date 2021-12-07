f0fd60bd095c321f636726be4ab87e9ed9b38a99
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-07 17:46:36 +0800

    Added functionality to get word occurrences of a user's input string into a dictionary.

--- Practical 05/word_occurrences.py
+++ Practical 05/word_occurrences.py
@@ -17,6 +17,29 @@ def main():
     print(BORDER)
     print("Practical 5: Question 3\nword_occurrences.py")
     print(BORDER)
+    input_text = input("Text: ").lower()
+    word_occurrences_dictionary = get_word_occurrences_dictionary(input_text)
+
+
+"""
+OTHER FUNCTIONS
+"""
+
+
+def get_word_occurrences_dictionary(input_text):
+    """ Get all word occurrences from a string input into a dictionary """
+    word_occurrences_dictionary = {}
+    word = ""
+    for char in input_text:
+        if char != " ":
+            word += char
+        else:
+            if word != "":
+                if word in word_occurrences_dictionary:
+                    word_occurrences_dictionary[word] += 1
+                else:
+                    word_occurrences_dictionary[word] = 1
+    return word_occurrences_dictionary
 
 
 """
