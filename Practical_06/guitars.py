09efce821177fa49fb2bed81d0cb5349224a0ba5
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-22 12:14:25 +0800

    Added `guitars.py` Python file into `Practical_06` directory.

---
+++ Practical_06/guitars.py
@@ -0,0 +1,27 @@
+"""
+Practical 6: Question 3C
+guitars.py
+"""
+
+
+# MODULES
+from Practical_06.guitar import Guitar
+
+# BORDER
+BORDER = "—" * 150
+
+
+"""
+PROGRAM
+"""
+
+
+def main():
+    """
+    Main program for adding guitars from users into a list
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
