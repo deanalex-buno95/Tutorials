a2482701405d05f45a0abcd821d7244b2f7c7b4a
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-22 10:54:29 +0800

    Added `__str__` method into `languages.py`.

--- Practical_06/programming_language.py
+++ Practical_06/programming_language.py
@@ -33,3 +33,9 @@ class ProgrammingLanguage:
         Check if the Programming Language is 'Dynamic'
         """
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """
+        Display Programming Language
+        """
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
