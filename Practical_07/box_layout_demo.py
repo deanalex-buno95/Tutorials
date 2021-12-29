ac42684bec1d665788af632feabda7654f59cede
deanb <deanalexander.buno@my.jcu.edu.au>
2021-12-29 11:00:13 +0800

    Added function `handle_greet()` into `.py` file.

--- Practical_07/box_layout_demo.py
+++ Practical_07/box_layout_demo.py
@@ -8,5 +8,8 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout_demo.kv')
         return self.root
 
+    def handle_greet(self):
+        print('greet')
+
 
 BoxLayoutDemo().run()
