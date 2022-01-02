c561e9ae0c25af608886ab70081a08fb3f70c17c
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-02 14:37:37 +0800

    Created the layout of the Kivy app for `dynamic_widgets.py` and `dynamic_widgets.kv`.

--- Practical_07/dynamic_widgets.py
+++ Practical_07/dynamic_widgets.py
@@ -2,3 +2,34 @@
 Practical 7: Question 4
 dynamic_widgets
 """
+
+# modules
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class DynamicWidgetsApp(App):
+    """
+    DynamicWidgetsApp is an App to create dynamic widgets for a dictionary of store items with their prices
+    """
+    status_text = StringProperty()
+
+    def __init__(self):
+        """
+        Constructor for the Kivy app
+        """
+        super().__init__()
+        self.item_to_price = {}  # dictionary to store items bought from a store with their prices
+
+    def build(self):
+        """
+        Build the Kivy GUI
+        """
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file("dynamic_widgets.kv")
+        return self.root
+
+
+DynamicWidgetsApp().run()
