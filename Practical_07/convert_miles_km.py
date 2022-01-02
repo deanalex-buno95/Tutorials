91af1c73d748d5983077bfe943ab5eaedae4c9d0
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-02 11:38:30 +0800

    Added build method for the app in the `.py` file and added basic layout on the `.kv` file for `convert_miles_km`.

--- Practical_07/convert_miles_km.py
+++ Practical_07/convert_miles_km.py
@@ -11,4 +11,9 @@ from kivy.core.window import Window
 
 class MilesKilometersConversionApp(App):
     """ MilesKilometersConversionApp is an App that converts a value in miles to kilometers """
-    pass
+    def build(self):
+        """ Build the Kivy app from the kv file """
+        Window.size = (1000, 500)
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
