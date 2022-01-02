ea1c7ebe7e48a3dd709023026a83f37f26d05de0
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-02 11:19:12 +0800

    Added `.py` and `.kv` files for `convert_miles_km`.

---
+++ Practical_07/convert_miles_km.py
@@ -0,0 +1,14 @@
+"""
+Practical 7: Question 3
+convert_miles_km
+"""
+
+# modules
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class MilesKilometersConversionApp(App):
+    """ MilesKilometersConversionApp is an App that converts a value in miles to kilometers """
+    pass
