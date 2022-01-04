fe7663af6f8fef9cb8dcad8010fcdec8043b25c8
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-04 20:51:35 +0800

    Added `taxi_simulator.py` into directory `Practical_08`.

---
+++ Practical_08/taxi_simulator.py
@@ -0,0 +1,17 @@
+"""
+Practical 8, Question 4
+taxi_simulator.py
+"""
+
+from silver_service_taxi import Taxi, SilverServiceTaxi
+
+BORDER = "—" * 150
+
+
+def main():
+    """ Taxi Simulator. """
+    pass
+
+
+if __name__ == '__main__':
+    main()
