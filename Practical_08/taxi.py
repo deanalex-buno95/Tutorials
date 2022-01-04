24b16dafc203302a067880a99aeed4bcd6c55529
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-04 18:40:09 +0800

    Edited `taxi.py`, to which the class variable `price_per_km` of value `1.23` is outside of the `__init__` constructor. Also edited `taxi_test.py` to fit the tests based on the new format.

--- Practical_08/taxi.py
+++ Practical_08/taxi.py
@@ -13,7 +13,7 @@ class Taxi(Car):
 
     def __init__(self, name, fuel):
         """
-        Initialise a Taxi instance,
+        Initialize a Taxi instance,
         based on parent class Car.
         """
         super().__init__(name, fuel)
