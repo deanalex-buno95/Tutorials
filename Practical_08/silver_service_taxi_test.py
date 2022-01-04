0196d15a47c387b1e815453d4bb91fe8d1d95d86
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-04 20:31:21 +0800

    Added `silver_service_taxi_test.py` in directory `Practical_08`.

---
+++ Practical_08/silver_service_taxi_test.py
@@ -0,0 +1,17 @@
+"""
+Practical 8, Question 3B
+silver_service_taxi_test.py
+"""
+
+from silver_service_taxi import SilverServiceTaxi
+
+BORDER = "—" * 150
+
+
+def main():
+    """ Test the SilverServiceTaxi class. """
+    pass
+
+
+if __name__ == '__main__':
+    main()
