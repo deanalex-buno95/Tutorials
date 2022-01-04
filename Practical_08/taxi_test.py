a056bda59be5f60a137ee4b3b95ba258339ec3bd
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-04 17:00:58 +0800

    Added `taxi_test.py` into directory `Practical_08`.

---
+++ Practical_08/taxi_test.py
@@ -0,0 +1,17 @@
+"""
+Practical 8: Question 1C
+taxi_test.py
+"""
+
+from taxi import Taxi
+
+BORDER = "—" * 150
+
+
+def main():
+    """ Test the Taxi class. """
+    pass
+
+
+if __name__ == '__main__':
+    main()
