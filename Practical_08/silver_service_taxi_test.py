3ea079821814cc5ede773288c11d4cfc3afd95e1
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-04 20:39:09 +0800

    Added tests in `silver_service_taxi_test.py`.

--- Practical_08/silver_service_taxi_test.py
+++ Practical_08/silver_service_taxi_test.py
@@ -10,7 +10,20 @@ BORDER = "—" * 150
 
 def main():
     """ Test the SilverServiceTaxi class. """
-    pass
+    print(BORDER)
+    print("Practical 8, Question 3B\nsilver_service_taxi_test.py")
+    print(BORDER)
+    print("Started…")
+    hummer = SilverServiceTaxi("Hummer", 200, 4)
+    super_taxi = SilverServiceTaxi("Super Taxi", 100, 2)
+    print(hummer)
+    print(super_taxi)
+    hummer.drive(0)
+    super_taxi.drive(18)
+    print(f"${hummer.get_fare():.2f}")  # $4.50
+    print(f"${super_taxi.get_fare():.2f}")  # $48.76 → $ $48.80
+    print("…Ended!")
+    print(BORDER)
 
 
 if __name__ == '__main__':
