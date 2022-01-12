0538f4476fce2073994c08d236f1496b6284e3a0
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-12 10:05:48 +0800

    Uncommented option 1 to rename files using `os.rename()` in `os_demos.py`.

--- Practical_09/os_demos.py
+++ Practical_09/os_demos.py
@@ -35,7 +35,7 @@ def main():
 
         # TODO: Try these options one at a time
         # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
+        os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
         # shutil.move(filename, 'temp/' + new_name)
