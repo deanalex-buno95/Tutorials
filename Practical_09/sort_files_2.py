1dbd0461e96d4fb8fb306ee6df96479b9ce6688a
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-16 18:47:55 +0800

    Add `sort_files_2.py` into directory `Practical_09`.

---
+++ Practical_09/sort_files_2.py
@@ -0,0 +1,41 @@
+"""
+Practical 9:
+Question 4
+sort_files_2.py
+"""
+
+
+import os
+import shutil
+
+
+def main():
+    """
+    Main function to change
+    :return: None
+    """
+    os.chdir("FilesToSort")  # change to directory `FilesToSort`
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):  # ignore directories, just process files
+            continue
+        file_extension = get_extension(filename)
+        try:  # new directory for new extension
+            os.mkdir(file_extension)
+        except FileExistsError:  # directory already exists
+            pass
+        print(f"Moving {filename} to {file_extension}")
+        shutil.move(filename, f"{file_extension}/{filename}")
+
+
+def get_extension(filename):
+    """
+    Get extension from filename
+    :return: extension (type: str)
+    """
+    necessary_portion = filename[-5:]
+    excess, extension = necessary_portion.split(".")
+    return extension
+
+
+if __name__ == '__main__':
+    main()
