0847ff0a01f83a7de705d5d23f12e985941c6795
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-16 18:58:50 +0800

    Add function `get_directory_name(file_extension, file_directories)` into `sort_files_2.py`.

--- Practical_09/sort_files_2.py
+++ Practical_09/sort_files_2.py
@@ -14,17 +14,21 @@ def main():
     Main function to change
     :return: None
     """
+    file_directories = {}
     os.chdir("FilesToSort")  # change to directory `FilesToSort`
     for filename in os.listdir('.'):
         if os.path.isdir(filename):  # ignore directories, just process files
             continue
         file_extension = get_extension(filename)
+        directory_name = get_directory_name(file_extension, file_directories)
+        '''
         try:  # new directory for new extension
             os.mkdir(file_extension)
         except FileExistsError:  # directory already exists
             pass
         print(f"Moving {filename} to {file_extension}")
         shutil.move(filename, f"{file_extension}/{filename}")
+        '''
 
 
 def get_extension(filename):
@@ -35,6 +39,16 @@ def get_extension(filename):
     necessary_portion = filename[-5:]
     excess, extension = necessary_portion.split(".")
     return extension
+
+
+def get_directory_name(file_extension, file_directories):
+    """
+    Get directory name from user
+    :param file_extension: string
+    :param file_directories: string
+    :return: directory_name (type: str)
+    """
+    pass
 
 
 if __name__ == '__main__':
