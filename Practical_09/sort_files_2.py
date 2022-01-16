444a87ea6d438fef53b0acea4e2fce9f17835faf
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-16 20:48:18 +0800

    Complete `sort_files_2.py`.

--- Practical_09/sort_files_2.py
+++ Practical_09/sort_files_2.py
@@ -8,19 +8,36 @@ sort_files_2.py
 import os
 import shutil
 
+SENTINEL = -1
+
 
 def main():
     """
     Main function to change
     :return: None
     """
-    file_directories = {}
+    file_categories = []
+    file_extensions_dictionary = {}
     os.chdir("FilesToSort")  # change to directory `FilesToSort`
     for filename in os.listdir('.'):
         if os.path.isdir(filename):  # ignore directories, just process files
             continue
-        file_extension = get_extension(filename)
-        directory_name = get_directory_name(file_extension, file_directories)
+        file_extension = get_extension(filename)  # get the extension
+        if file_extension not in file_extensions_dictionary:  # extension not in dictionary
+            directory_name = get_directory_name(file_extension)  # set directory name as value of extension
+            file_categories.append(directory_name)
+            try:  # first time
+                os.mkdir(directory_name)
+            except FileExistsError:  # subsequent times
+                pass
+            file_extensions_dictionary[file_extension] = directory_name
+            print(f"Moving {filename} to {directory_name}")
+            shutil.move(filename, f"{directory_name}/{filename}")
+        else:  # extension in dictionary
+            directory_name = file_extensions_dictionary[file_extension]
+            print(f"Moving {filename} to {directory_name}")
+            shutil.move(filename, f"{directory_name}/{filename}")
+
         '''
         try:  # new directory for new extension
             os.mkdir(file_extension)
@@ -41,14 +58,18 @@ def get_extension(filename):
     return extension
 
 
-def get_directory_name(file_extension, file_directories):
+def get_directory_name(file_extension):
     """
     Get directory name from user
     :param file_extension: string
-    :param file_directories: string
     :return: directory_name (type: str)
     """
-    pass
+    run = 0
+    while run != SENTINEL:
+        directory_name = input(f"What category would you like to sort {file_extension} files into? ")
+        if directory_name != "":
+            return directory_name
+        print("Invalid input!")
 
 
 if __name__ == '__main__':
