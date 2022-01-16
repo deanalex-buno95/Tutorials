3d4a7e4a5a493a3755081f9ff000555ecb8cffff
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-16 18:10:16 +0800

    Make `main()` function in `sort_files_1.py` to print each file name with its extension for checking.

--- Practical_09/sort_files_1.py
+++ Practical_09/sort_files_1.py
@@ -5,12 +5,23 @@ sort_files_1.py
 """
 
 
+import os
+
+
 def main():
     """
     Main function to change
     :return: None
     """
-    pass
+    os.chdir("FilesToSort")  # change to directory `FilesToSort`
+    for directory_name, subdirectories, filenames in os.walk('.'):  # walk through the filenames in directory
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+        for filename in filenames:  # loop each file name for moving files into created directories
+            file_extension = get_extension(filename)
+            print(filename, file_extension)
 
 
 def get_extension(filename):
