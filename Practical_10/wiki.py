e76fe2d532bdfce345b4b1957a4ac922055673f6
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-18 09:55:13 +0800

    Add functions `main()` and `get_searched_page()` into `wiki.py`.

--- Practical_10/wiki.py
+++ Practical_10/wiki.py
@@ -5,3 +5,24 @@ wiki.py
 """
 
 
+import wikipedia
+
+
+# CONSTANTS
+BORDER = "—" * 150
+SENTINEL = -1
+
+
+def main():
+    """
+    Main function for the script
+    :return: None
+    """
+    user_page = get_search_page()
+    print(user_page.summary())
+
+
+def get_search_page():
+    user_input = input("Search: ")
+    user_pages = wikipedia.search(user_input)
+
