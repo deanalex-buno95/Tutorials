0a787083374a1cf9f49dda00d71898bcbeb3d4a2
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-29 13:53:01 +0800

    Modify and test function `get_word_format` in Python file `word_generator.py` with new format system.

--- Practical_02/word_generator.py
+++ Practical_02/word_generator.py
@@ -9,7 +9,12 @@ import random
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+LETTERS = VOWELS + CONSONANTS
+WILDCARDS = "%#*"
+FORMAT_CHARACTERS = LETTERS + WILDCARDS
 SENTINEL = -1
+LOWER = 5
+UPPER = 10
 
 
 def get_word_format():
@@ -18,8 +23,8 @@ def get_word_format():
     while run != SENTINEL:
         input_format = input("Format: ").lower()
         valid_format = True
-        for char in input_format:
-            if char not in ["c", "v"]:
+        for character in input_format:
+            if character not in [char for char in FORMAT_CHARACTERS]:
                 valid_format = False
         if valid_format:
             return input_format
@@ -29,10 +34,29 @@ def get_word_format():
 
 word_format = get_word_format()
 word = ""
-for kind in word_format:
-    if kind == "c":
+for format_char in word_format:
+    if format_char == "%":
         word += random.choice(CONSONANTS)
-    else:
+    elif format_char == "#":
         word += random.choice(VOWELS)
-
+    elif format_char == "*":
+        word += random.choice(LETTERS)
+    else:
+        word += format_char
 print(word)
+
+random_word_format = ""
+for i in range(random.randint(LOWER, UPPER+1)):
+    random_word_format += random.choice(FORMAT_CHARACTERS)
+print(f"Format: {random_word_format}")
+random_word = ""
+for format_char in random_word_format:
+    if format_char == "%":
+        random_word += random.choice(CONSONANTS)
+    elif format_char == "#":
+        random_word += random.choice(VOWELS)
+    elif format_char == "*":
+        random_word += random.choice(LETTERS)
+    else:
+        random_word += format_char
+print(random_word)
