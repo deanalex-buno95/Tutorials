e76299d70fd5cd37fc79cc06bd712311a6e10929
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-29 13:28:34 +0800

    Add Python file `word_generator.py` in directory `Practical_02`.

---
+++ Practical_02/word_generator.py
@@ -0,0 +1,21 @@
+"""
+Practice
+word_generator.py
+"""
+
+
+import random
+
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+word_format = "ccvcvvc"
+word = ""
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+
+print(word)
