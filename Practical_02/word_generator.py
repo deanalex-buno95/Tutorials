e274940c32ab146e1e0aabab459dfcb9a993b9ec
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-29 13:39:26 +0800

    Test function `get_word_format` in Python file `word_generator.py`.

--- Practical_02/word_generator.py
+++ Practical_02/word_generator.py
@@ -9,11 +9,22 @@ import random
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+SENTINEL = -1
 
 
 def get_word_format():
     """ Get word format from user. """
-    pass
+    run = 0
+    while run != SENTINEL:
+        input_format = input("Format: ").lower()
+        valid_format = True
+        for char in input_format:
+            if char not in ["c", "v"]:
+                valid_format = False
+        if valid_format:
+            return input_format
+        else:
+            print("Invalid format! Try again!")
 
 
 word_format = get_word_format()
