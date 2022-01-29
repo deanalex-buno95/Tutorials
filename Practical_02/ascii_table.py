bed351b96f2aaa9d631962a11f44dda4768c6046
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-29 12:48:11 +0800

    Add function `get_character` into Python file `ascii_table.py`.

--- Practical_02/ascii_table.py
+++ Practical_02/ascii_table.py
@@ -4,8 +4,17 @@ ascii_table.py
 """
 
 
+LOWER = 33
+UPPER = 127
+
+
 def main():
     """ Main function for the program. """
+    character_input = get_character()
+
+
+def get_character():
+    """ Get a character from the user. """
     pass
 
 
