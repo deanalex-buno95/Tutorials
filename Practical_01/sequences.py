e8b55f51a29ce285b49b297b0ef1d085ec67bb1c
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-29 11:53:29 +0800

    Add CONSTANTS `MENU`, `OPTIONS`, and `SENTINEL` into Python file `sequences.py`.

--- Practical_01/sequences.py
+++ Practical_01/sequences.py
@@ -4,6 +4,11 @@ sequences.py
 """
 
 
+MENU = "1. Even numbers from x to y\n2. Odd numbers from x to y\n3. Squares from x to y\n4. Quit"
+OPTIONS = [1, 2, 3, 4]
+SENTINEL = -1
+
+
 def main():
     """ Main function for the sequences menu. """
     pass
