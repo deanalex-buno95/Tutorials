81f8daa646e44938d2f63a97993e1ab5e5832325
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-30 15:38:46 +0800

    Add Python file `add_memberwise.py` into directory `Practical_04`.

---
+++ Practical_04/add_memberwise.py
@@ -0,0 +1,18 @@
+"""
+Practice:
+add_memberwise
+"""
+
+
+def main():
+    """ Main program. """
+    pass
+
+
+def add_memberwise(list_1, list_2):
+    """ Add values of two lists, memberwise. """
+    pass
+
+
+if __name__ == "__main__":
+    main()
