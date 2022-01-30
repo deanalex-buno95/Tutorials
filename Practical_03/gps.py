52ebddf19810c3464c25a3641562a9ddcd4d968b
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-30 14:46:24 +0800

    Add Python file `gps.py` into directory `Practical_03`.

---
+++ Practical_03/gps.py
@@ -0,0 +1,24 @@
+"""
+Practice:
+gps.py
+"""
+
+
+import random
+
+
+STARTING_POPULATION = 1000
+MINIMUM_BORN_DECIMAL = 0.1
+MAXIMUM_BORN_DECIMAL = 0.2
+MINIMUM_DEATH_DECIMAL = 0.05
+MAXIMUM_DEATH_DECIMAL = 0.25
+NUMBER_OF_YEARS = 10
+
+
+def main():
+    """ Main function for the GPS program. """
+    pass
+
+
+if __name__ == "__main__":
+    main()
