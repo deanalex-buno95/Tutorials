d24952eb8e4391c5f8fd718972812952dde87f24
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-31 17:37:57 +0800

    Add Python file `test_date.py` into directory `Practical_06`.

---
+++ Practical_06/test_date.py
@@ -0,0 +1,16 @@
+"""
+Practice:
+test_date.py
+"""
+
+
+from Practical_06.date import Date
+
+
+def main():
+    """ Main function for testing. """
+    pass
+
+
+if __name__ == "__main__":
+    main()
