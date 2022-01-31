b103c761e44e519b1a4efc05614f8473a505f5ca
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-31 17:08:28 +0800

    Add Python file `test_person.py` into directory `Practical_06`.

---
+++ Practical_06/test_person.py
@@ -0,0 +1,16 @@
+"""
+Practice:
+test_person.py
+"""
+
+
+from Practical_06.person import Person
+
+
+def main():
+    """ Main program for testing. """
+    pass
+
+
+if __name__ == "__main__":
+    main()
