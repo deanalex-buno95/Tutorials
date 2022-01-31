d6f08ef9f022515b41d9bea7edaf4906c1647bd3
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-31 17:57:39 +0800

    Test Python file `date.py` with Python file `test_date.py` into directory `Practical_06`.

--- Practical_06/date.py
+++ Practical_06/date.py
@@ -9,4 +9,19 @@ import datetime
 
 class Date:
     """ Date is class for any instance of a date. """
-    pass
+    def __init__(self, day=0, month=0, year=0):
+        """ Constructor for Date. """
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self):
+        """ Display Date. """
+        return f"{self.year}/{self.month}/{self.day}"
+
+    def add_days(self, n):
+        """ Add days to Date. """
+        updated_date = datetime.date(self.year, self.month, self.day) + datetime.timedelta(days=n)
+        self.day = updated_date.day
+        self.month = updated_date.month
+        self.year = updated_date.year
--- Practical_06/test_date.py
+++ Practical_06/test_date.py
@@ -4,12 +4,26 @@ test_date.py
 """
 
 
+import datetime
 from Practical_06.date import Date
+
+NUMBER_OF_DAYS = 5
 
 
 def main():
     """ Main function for testing. """
-    pass
+    print("Test begun…")
+    empty_date = Date()  # 0/0/0
+    print(empty_date)
+    today = datetime.date.today()  # current date
+    current_day = today.day
+    current_month = today.month
+    current_year = today.year
+    date = Date(current_day, current_month, current_year)  # general date
+    print(date)
+    date.add_days(NUMBER_OF_DAYS)  # `NUMBER_OF_DAYS` of days later
+    print(date)
+    print("… Test ended!")
 
 
 if __name__ == "__main__":
