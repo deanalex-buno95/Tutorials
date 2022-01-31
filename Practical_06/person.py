eed5427f845eb5433b89649e6c4fd8aba1ee5991
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-31 17:24:43 +0800

    Test Python file `person.py` with Python file `test_person.py` into directory `Practical_06`.

--- Practical_06/person.py
+++ Practical_06/person.py
@@ -6,4 +6,36 @@ person.py
 
 class Person:
     """ Person is a class that describes an instance of a person. """
-    pass
+    def __init__(self, first_name="", last_name="", age=0):
+        """ Constructor for Person. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def get_first_name(self):
+        """ Getter for Person.first_name. """
+        return self.first_name
+
+    def get_last_name(self):
+        """ Getter for Person.last_name. """
+        return self.last_name
+
+    def get_age(self):
+        """ Getter for Person.age. """
+        return self.age
+
+    def set_first_name(self, new_first_name):
+        """ Setter for Person.first_name. """
+        self.first_name = new_first_name
+
+    def set_last_name(self, new_last_name):
+        """ Setter for Person.last_name. """
+        self.last_name = new_last_name
+
+    def set_age(self, new_age):
+        """ Setter for Person.age. """
+        self.age = new_age
+
+    def __str__(self):
+        """ Display Person. """
+        return f"{self.get_last_name()}, {self.get_first_name()}. Age: {self.get_age()}."
--- Practical_06/test_person.py
+++ Practical_06/test_person.py
@@ -9,7 +9,19 @@ from Practical_06.person import Person
 
 def main():
     """ Main program for testing. """
-    pass
+    print("Test begun…")
+    empty_person = Person()  # test person with default parameters
+    print(empty_person)  # ", . Age: 0."
+    actual_person = Person("Lauri", "Törni", 46)  # test person with filled parameters
+    print(actual_person)  # "Törni, Lauri. Age: 46."
+    print(actual_person.get_first_name())  # "Lauri"
+    print(actual_person.get_last_name())  # "Törni"
+    print(actual_person.get_age())  # 46
+    actual_person.set_first_name("Larry")  # change first name from "Lauri" to "Larry"
+    actual_person.set_last_name("Thorne")  # change last name from "Törni" to "Thorne"
+    actual_person.set_age(103)
+    print(actual_person)  # "Thorne, Lauri. Age: 103"
+    print("… Test ended!")
 
 
 if __name__ == "__main__":
