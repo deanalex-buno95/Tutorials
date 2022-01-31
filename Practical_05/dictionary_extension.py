59a9f0db0b904b6c9d5b153fcc9de80df249db65
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-31 16:30:32 +0800

    Add Python file `dictionary_extension.py` into directory `Practical_05`.

---
+++ Practical_05/dictionary_extension.py
@@ -0,0 +1,21 @@
+"""
+Practice:
+dictionary_extension.py
+"""
+
+
+NUMBER_OF_PEOPLE = 5
+
+
+def main():
+    """ Main function for program. """
+    pass
+
+
+def get_dob_dictionary(name_list, dob_list):
+    """ Get date-of-birth dictionary from two lists: name_list and dob_list. """
+    pass
+
+
+if __name__ == "__main__":
+    main()
